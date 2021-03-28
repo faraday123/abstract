86b10f4cd29a233c84ad50492bedb6bc4ee06a1a
samuel-adjin <samuel.adjin@amalitech.org>
2021-03-28 11:26:56 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.Amalitech;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ NodeList.java
@@ -0,0 +1,9 @@
+package com.Amalitech;
+
+public interface NodeList {
+    abstract ListItem getRoot() ;
+    abstract boolean addItem(ListItem item);
+    abstract boolean removeItem(ListItem item);
+    abstract void traverse(ListItem root);
+
+}
